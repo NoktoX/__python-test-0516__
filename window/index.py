10f41b16eabbef1586d38985bf2f9b0249351f54
NoktoX <sherry.chen77@gmail.com>
2024-05-28 14:00:32 +0800

    modify

--- window/index.py
+++ window/index.py
@@ -1,9 +1,18 @@
+import tkinter as tk
+
 def get_names() -> list[str]:
     with open('names.txt',encoding='utf-8') as file:
         content:str = file.read()
     names:list[str] = content.split()
     return names
 
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        #多做一些事
+        self.title("我的第一個GUI程式")
+
 if __name__ == '__main__':
     names:list[str] = get_names()
-    print(names)
+    window:Window = Window()    
+    window.mainloop()
