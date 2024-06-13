5d3b1834f5f10cf29031a15e84ae5fb287e93408
NoktoX <sherry.chen77@gmail.com>
2024-06-13 10:14:09 +0800

    update_class_0611

--- 2024_06_11_01/index.py
+++ 2024_06_11_01/index.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from ttkthemes import ThemedTk
 from tkinter import messagebox
+from tools import CustomMessagebox
 
 class Window(tk.Tk):
     def __init__(self,theme:str|None,**kwargs):
@@ -14,46 +15,61 @@ class Window(tk.Tk):
         # print(style.theme_names())
         style.configure('input.TFrame',background='#ffffff')
         style.configure('press.TButton',font=('arial',16))
-
+        #========================
         titleFrame = ttk.Frame(self)
         title_label = ttk.Label(self, text = "BMI計算器", font=("Arial",20))
         title_label.pack(pady=10)
         titleFrame.pack(padx=100,pady=(0,20))
+        #========================
 
         # 建立輸入區的特殊背景框
-        input_frame = ttk.Frame(self,style='input.TFrame')
+        input_frame = ttk.Frame(self,style='Input.TFrame')
 
         # 姓名
         label_name = ttk.Label(input_frame, text="姓名:")
         label_name.grid(row=0, column=0, padx=5, pady=5,sticky=tk.E)
 
-        entry_name = ttk.Entry(input_frame)
+        self.name_value = tk.StringVar()
+        self.name_value.set('')
+        entry_name = ttk.Entry(input_frame,textvariable=self.name_value)
         entry_name.grid(row=0, column=1, padx=5, pady=5)
 
         # 身高體重
         label_height = ttk.Label(input_frame, text="身高 (cm):")
         label_height.grid(row=1, column=0, padx=5, pady=5,sticky=tk.E)
 
-        entry_height = ttk.Entry(input_frame)
+        self.height_value = tk.StringVar()
+        self.height_value.set('')
+        entry_height = ttk.Entry(input_frame,textvariable=self.height_value)
         entry_height.grid(row=1, column=1, padx=5, pady=5)
 
         label_weight = ttk.Label(input_frame, text="體重 (kg):")
         label_weight.grid(row=2, column=0, padx=5, pady=5,sticky=tk.E)
 
-        entry_weight = ttk.Entry(input_frame)
-        entry_weight.grid(row=2, column=1, padx=5, pady=5)
+        self.weight_value = tk.StringVar()
+        self.weight_value.set('') 
+        entry_weight = ttk.Entry(input_frame,textvariable=self.weight_value)
+        entry_weight.grid(row=2, column=1, padx=5, pady=5)    
 
         input_frame.pack(pady=10,padx=30)
+        #========================
+
 
         # 計算按鈕設計
-        button_calculate = ttk.Button(self, text="計算", command=self.show_bmi_result,style='press.TButton')
-        button_calculate.pack(side=tk.RIGHT,padx=(0,35),pady=10,ipadx=10,ipady=15)
+        button_frame = ttk.Frame(self)
+        button_calculate = ttk.Button(button_frame, text="計算", command=self.show_bmi_result,style='press.TButton')
+        button_calculate.pack(side=tk.RIGHT,expand=True,fill=tk.X)
+
+        button_close = ttk.Button(button_frame,text="關閉",command=self.destroy,style='press.TButton')
+        button_close.pack(side=tk.LEFT,expand=True,fill=tk.X)
+        button_frame.pack(padx=20,fill=tk.X,pady=(0,15))
+
 
     def show_bmi_result(self):
         try:
-            name:str = self.entry_name.get()
-            height:int = int(self.entry_height.get())
-            weight:int = int(self.entry_weight.get())
+            name:str = self.name_value.get()
+            height:int = int(self.height_value.get())
+            weight:int = int(self.weight_value.get())
         
         #except UnboundLocalError:
             #messagebox.showwarning("Warning","欄位沒有填寫")
@@ -61,16 +77,11 @@ class Window(tk.Tk):
             messagebox.showwarning("Warning","格式錯誤,欄位沒有填寫")
         except Exception:
             messagebox.showwarning("Warning","不知明的錯誤")
-        
         else:
-            self.show_bmi_result(name=name,height=height,weight=weight)
+            self.show_result(name=name,height=height,weight=weight)
         # print(name,height,weight)
 
-def show_bmi_result(self,name:str,height:int,weight:int):
-    try:
-        name = entry_name.get()
-        height = float(entry_height.get())
-        weight = float(entry_weight.get())
+    def show_result(self,name:str,height:int,weight:int):
         bmi = weight / (height / 100) ** 2
         if bmi < 18.5:
             status = "體重過輕"
@@ -78,7 +89,7 @@ def show_bmi_result(self,name:str,height:int,weight:int):
             weight_change = ideal_weight - weight
             status_color = "red"
             advice = f"您需要至少增加 {abs(weight_change):.2f} 公斤才能達到正常體重。"
-        elif 18.5 <= bmi <= 24.9:
+        elif 18.5 <= bmi < 24.9:
             status = "正常"
             status_color = "black"
             advice = "您的體重正常，請保持！"
@@ -88,18 +99,12 @@ def show_bmi_result(self,name:str,height:int,weight:int):
             weight_change = weight - ideal_weight
             status_color = "red"
             advice = f"您需要至少減少 {abs(weight_change):.2f} 公斤才能達到正常體重。"
-        
-        result_message = f"{name}您好:\n   bmi:{bmi:.2f}\n   體重:{status}\n   建議:{advice}"
-        
-        # 顯示自定義的msgbox
-        if status == "體重過輕" or status == "體重過重":
-            dialog = CustomMessagebox(root, title="BMI結果", message=result_message)
-        else:
-            messagebox.showinfo("BMI結果", result_message)
 
-    except ValueError:
-        messagebox.showerror("輸入無效", "請輸入有效的身高和體重數值。")
 
+        CustomMessagebox(self,title="BMI",name=name,bmi=bmi,status=status,advice=advice,status_color=status_color)
+
+    def __repr__(self):
+        return "我是window的實體"
         
 def main():
     window = Window(theme="ace")
